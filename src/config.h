5f42be53d242e4098b6097015dc469a3362a18fa
Florian Löw <florian.loew@edu.tbz.ch>
2023-12-21 14:38:42 +0100

    add integration logic

--- src/config.h
+++ src/config.h
@@ -1,6 +1,13 @@
 #define INTEGRATION
 
+// Always
+#define INTEGRATION_AUTOMATIC_MODE false
+
 #define SERIAL_BAUD_RATE 115200
 
-// Pin definitions
+// Pin definitions for the user interface module
 #define CONFIG_BUTTON_PIN 0
+
+// Pin definitions for the elevator integration module
+#define LIGHT_SENSOR_PIN 35
+#define SERVO_PIN 32
